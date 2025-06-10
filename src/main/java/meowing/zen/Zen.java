6c7c48b1c5e791ebbc952c58b1e59714145589bd
kiwidotzip <snowykiwii@gmail.com>
2025-06-10 17:44:26 +0530

    Add /zen and remove the keybind

--- src/main/java/meowing/zen/Zen.java
+++ src/main/java/meowing/zen/Zen.java
@@ -1,21 +1,19 @@
 package meowing.zen;
 
 import net.fabricmc.api.ClientModInitializer;
-import meowing.zen.feats.meowing.automeow;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
-import meowing.zen.config.zencfg;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
+
+import meowing.zen.feats.meowing.automeow;
+import meowing.zen.config.zencfg;
+import meowing.zen.utils.TickScheduler;
 
 import java.util.Objects;
 
 public class Zen implements ClientModInitializer {
-	public static KeyBinding openConfigKey;
 	private static long startTime;
 	private static int moduleCount = 0;
 	private static boolean shown = false;
@@ -26,19 +24,19 @@ public class Zen implements ClientModInitializer {
 
 		automeow.initialize();
 		moduleCount++;
-
 		zencfg.Handler.load();
 
-		openConfigKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"key.zen.open_config",
-				InputUtil.Type.KEYSYM,
-				GLFW.GLFW_KEY_Z,
-				"category.zen"
-		));
-
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (openConfigKey.wasPressed())
-				MinecraftClient.getInstance().setScreen(zencfg.createConfigScreen(MinecraftClient.getInstance().currentScreen));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("zen")
+					.executes(context -> {
+						TickScheduler.register();
+						TickScheduler.schedule(1, () -> {
+							MinecraftClient client = MinecraftClient.getInstance();
+							client.execute(() -> client.setScreen(zencfg.createConfigScreen(client.currentScreen)));
+						});
+						return 1;
+					})
+			);
 		});
 
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
