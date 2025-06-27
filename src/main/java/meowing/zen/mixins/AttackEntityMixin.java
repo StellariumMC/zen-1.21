0b8e3a7ea372aaeb4e46c8e81590d34f15f7ed23
kiwidotzip <snowykiwii@gmail.com>
2025-06-27 16:07:58 +0530

    Remove useless import

--- src/main/java/meowing/zen/mixins/AttackEntityMixin.java
+++ src/main/java/meowing/zen/mixins/AttackEntityMixin.java
@@ -2,11 +2,9 @@ package meowing.zen.mixins;
 
 import meowing.zen.events.AttackEntityEvent;
 import meowing.zen.events.EventBus;
-import meowing.zen.utils.ChatUtils;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.ClickEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
