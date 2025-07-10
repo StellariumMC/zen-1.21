76cacea0c0571e6a2ab1fca99e290b0107aaa5fb
kiwidotzip <snowykiwii@gmail.com>
2025-07-10 19:22:56 +0530

    Revert to manual feature loader - reflections is too big of a pain in the ass

--- src/main/kotlin/meowing/zen/feats/FeatureLoader.kt
+++ src/main/kotlin/meowing/zen/feats/FeatureLoader.kt
@@ -6,36 +6,65 @@ import meowing.zen.feats.carrying.carrycommand
 import meowing.zen.feats.general.CalculatorCommand
 import meowing.zen.feats.slayers.SlayerStatsCommand
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
-import org.reflections.Reflections
 
 object FeatureLoader {
+    private val features = arrayOf(
+        "meowing.automeow",
+        "meowing.meowdeathsounds",
+        "meowing.meowsounds",
+        "meowing.meowmessage",
+        "general.guildjoinleave",
+        "general.friendjoinleave",
+        "general.guildmessage",
+        "general.partymessage",
+        "general.worldage",
+        "general.betterah",
+        "general.betterbz",
+        "general.blockoverlay",
+        "general.customsize",
+        "general.serveralert",
+        "slayers.MetadataHandler",
+        "slayers.slayertimer",
+        "slayers.slayerhighlight",
+        "slayers.slayerstats",
+        "slayers.vengdmg",
+        "slayers.vengtimer",
+        "slayers.lasertimer",
+        "slayers.minibossspawn",
+        "carrying.carrycounter",
+        "dungeons.bloodtimer",
+        "dungeons.cryptreminder",
+        "dungeons.keyalert",
+        "dungeons.keyhighlight",
+        "dungeons.partyfinder",
+        "dungeons.serverlagtimer",
+        "dungeons.termtracker",
+        "dungeons.firefreeze",
+        "dungeons.architectdraft",
+        "dungeons.boxstarmobs",
+        "dungeons.leapannounce",
+        "noclutter.hidefallingblocks",
+        "noclutter.hidefireoverlay",
+        "noclutter.hidestatuseffects",
+        "noclutter.nothunder"
+    )
+
     private var moduleCount = 0
     private var moduleErr = 0
     private var loadtime: Long = 0
 
     fun init() {
         val starttime = System.currentTimeMillis()
-
-        try {
-            val reflections = Reflections("meowing.zen.feats")
-            val featureClasses = reflections.getSubTypesOf(Feature::class.java)
-
-            featureClasses.forEach { clazz ->
-                try {
-                    val constructor = clazz.getDeclaredConstructor()
-                    constructor.isAccessible = true
-                    constructor.newInstance()
-                    moduleCount++
-                } catch (e: Exception) {
-                    System.err.println("[Zen] Error initializing ${clazz.simpleName}: $e")
-                    e.printStackTrace()
-                    moduleErr++
-                }
+        features.forEach { className ->
+            try {
+                val fullClassName = "meowing.zen.feats.$className"
+                Class.forName(fullClassName)
+                moduleCount++
+            } catch (e: Exception) {
+                System.err.println("[Zen] Error initializing $className: $e")
+                e.printStackTrace()
+                moduleErr++
             }
-        } catch (e: Exception) {
-            System.err.println("[Zen] Error during reflection scan: $e")
-            e.printStackTrace()
-            moduleErr++
         }
 
         ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
