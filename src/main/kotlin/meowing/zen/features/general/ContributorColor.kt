f3b67c9a19e94f284f182f580db31f8fae84b085
aurielyn <aurielyn.dev@gmail.com>
2025-08-21 21:56:51 +0530

    Fix PetTracker creating 5 bazillion instances of DataUtils

--- src/main/kotlin/meowing/zen/features/general/ContributorColor.kt
+++ src/main/kotlin/meowing/zen/features/general/ContributorColor.kt
@@ -47,8 +47,8 @@ object ContributorColor {
             },
             onError = {
                 contributorData = mapOf(
-                    "shikiimori" to ContributorInfo("§dKiwi§r", Color(255, 0, 255, 127).toColorInt(), listOf(255, 0, 255, 127)),
-                    "cheattriggers" to ContributorInfo("§cCheater§r", Color(255, 0, 0, 127).toColorInt(), listOf(255, 0, 0, 127)),
+                    "aurielyn" to ContributorInfo("§daurielyn§r", Color(255, 0, 255, 127).toColorInt(), listOf(255, 0, 255, 127)),
+                    "cheattriggers" to ContributorInfo("§cKiwi§r", Color(255, 0, 0, 127).toColorInt(), listOf(255, 0, 0, 127)),
                     "Aur0raDye" to ContributorInfo("§5Mango 6 7§r", Color(170, 0, 170, 127).toColorInt(), listOf(170, 0, 170, 127)),
                     "Skyblock_Lobby" to ContributorInfo("§9Skyblock_Lobby§r", Color(85, 85, 255, 127).toColorInt(), listOf(85, 85, 255, 127))
                 )
