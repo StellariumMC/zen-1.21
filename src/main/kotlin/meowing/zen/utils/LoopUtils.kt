1a9c7388e6a274e6c243acf58d72f09ea6b7bb7b
aurielyn <aurielyn.dev@gmail.com>
2025-08-22 09:55:31 +0530

    Add loopDynamic | Add RandomMeow | Fix loops always being registered until you toggled the feature at least once every launch

--- src/main/kotlin/meowing/zen/utils/LoopUtils.kt
+++ src/main/kotlin/meowing/zen/utils/LoopUtils.kt
@@ -6,24 +6,46 @@ import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
 object LoopUtils {
-    private val executor = Executors.newScheduledThreadPool(2)
+    private val timerExecutor = Executors.newScheduledThreadPool(1)
     private val tasks = ConcurrentHashMap<String, ScheduledFuture<*>>()
 
-    fun setTimeout(delay: Long, callback: Runnable) = executor.schedule(callback, delay, TimeUnit.MILLISECONDS)
+    fun setTimeout(delay: Long, callback: Runnable) = timerExecutor.schedule(callback, delay, TimeUnit.MILLISECONDS)
 
     fun loop(delay: Long, stop: () -> Boolean = { false }, func: () -> Unit): String {
         val id = "${System.nanoTime()}"
-        scheduleLoop(id, { delay }, stop, func)
+        scheduleLoop(id, delay, stop, func)
         return id
     }
 
     fun loop(delay: () -> Number, stop: () -> Boolean = { false }, func: () -> Unit): String {
         val id = "${System.nanoTime()}"
-        scheduleLoop(id, { delay().toLong() }, stop, func)
+        scheduleLoop(id, delay().toLong(), stop, func)
         return id
     }
 
-    fun removeLoop(id: String) = tasks.remove(id)?.cancel(false) ?: false
+    fun loopDynamic(delay: () -> Long, stop: () -> Boolean = { false }, func: () -> Unit): String {
+        val id = "${System.nanoTime()}"
+        scheduleDynamicLoop(id, delay, stop, func)
+        return id
+    }
+
+    private fun scheduleDynamicLoop(id: String, delay: () -> Long, stop: () -> Boolean, func: () -> Unit) {
+        fun scheduleNext() {
+            if (!stop()) {
+                val task = Runnable {
+                    try {
+                        func()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                    scheduleNext()
+                }
+                tasks[id] = timerExecutor.schedule(task, delay(), TimeUnit.MILLISECONDS)
+            } else tasks.remove(id)
+        }
+        scheduleNext()
+    }
 
     private fun scheduleLoop(id: String, delay: () -> Long, stop: () -> Boolean, func: () -> Unit) {
         val task = Runnable {
@@ -32,14 +54,16 @@ object LoopUtils {
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-            if (!stop()) {
-                tasks[id] = executor.schedule({
+            if (!stop())
+                tasks[id] = timerExecutor.schedule({
                     scheduleLoop(id, delay, stop, func)
                 }, delay(), TimeUnit.MILLISECONDS)
-            } else {
-                tasks.remove(id)
-            }
+            else tasks.remove(id)
         }
-        tasks[id] = executor.schedule(task, 0, TimeUnit.MILLISECONDS)
+        tasks[id] = timerExecutor.schedule(task, 0, TimeUnit.MILLISECONDS)
     }
+
+    fun removeLoop(id: String) = tasks.remove(id)?.cancel(false) ?: false
+
+    private fun scheduleLoop(id: String, delay: Long, stop: () -> Boolean, func: () -> Unit) = scheduleLoop(id, { delay }, stop, func)
 }
