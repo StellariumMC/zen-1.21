b96badc65962fa4e8cb4684f88f5e28e3e8a3fd0
benji <cg125892@gmail.com>
2025-10-24 17:30:47 +0100

    refactor: migrate bat death detection from packet events to entity events

--- src/main/kotlin/xyz/meowing/zen/features/dungeons/BatDeathTitle.kt
+++ src/main/kotlin/xyz/meowing/zen/features/dungeons/BatDeathTitle.kt
@@ -3,12 +3,13 @@ package xyz.meowing.zen.features.dungeons
 import xyz.meowing.zen.Zen
 import xyz.meowing.zen.config.ConfigManager
 import xyz.meowing.zen.config.ui.types.ElementType
-import xyz.meowing.zen.events.PacketEvent
 import xyz.meowing.zen.features.Feature
-import xyz.meowing.zen.utils.TitleUtils.showTitle
-import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket
 import xyz.meowing.zen.config.ConfigElement
-import xyz.meowing.zen.utils.LocationUtils.subarea
+import xyz.meowing.zen.events.EntityEvent
+import xyz.meowing.zen.utils.LocationUtils
+import xyz.meowing.zen.utils.TitleUtils
+import net.minecraft.entity.decoration.ArmorStandEntity
+import net.minecraft.entity.passive.BatEntity
 
 @Zen.Module
 object BatDeathTitle : Feature("batdeadtitle", true, "catacombs") {
@@ -22,12 +23,10 @@ object BatDeathTitle : Feature("batdeadtitle", true, "catacombs") {
     }
 
     override fun initialize() {
-        register<PacketEvent.Received> { event ->
-            if (event.packet is PlaySoundS2CPacket) {
-                val packet = event.packet
-                if (!packet.sound.toString().contains("minecraft:entity.bat.death") && !packet.sound.toString().contains("minecraft:entity.bat.hurt")) return@register
-                if (subarea?.lowercase()?.contains("boss") == true) return@register
-                showTitle("§cBat Dead!", null, 1000)
+        register<EntityEvent.Death> {
+            if (it.entity is BatEntity && it.entity.vehicle !is ArmorStandEntity &&
+                LocationUtils.subarea?.lowercase()?.contains("boss") != true) {
+                TitleUtils.showTitle("§cBat Dead!", null, 1000)
             }
         }
     }
