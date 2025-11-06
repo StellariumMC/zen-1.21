beb93c3ffa28e0b2972e36bc0204250bfe2f914f
aurielyn <aurielyn.dev@gmail.com>
2025-11-06 16:46:31 +0530

    Misc: sort sections in panels by name

--- src/main/kotlin/xyz/meowing/zen/config/ui/panels/Panel.kt
+++ src/main/kotlin/xyz/meowing/zen/config/ui/panels/Panel.kt
@@ -108,7 +108,7 @@ class Panel(
             true
         }
 
-        category.features.forEachIndexed { index, feature ->
+        category.features.sortedBy { it.featureName }.forEachIndexed { index, feature ->
             val isLast = index == category.features.size - 1
             sections.add(SectionButton(feature, sectionsContainer, onConfigUpdate, isLast))
         }
